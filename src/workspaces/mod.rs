0a6c168e624f661050fb2c8f2659b1e6e6bd5bd2
dyegomb <dyegomb@gmail.com>
2024-02-20 16:00:46 -0300

    testes

--- src/workspaces/mod.rs
+++ src/workspaces/mod.rs
@@ -4,38 +4,52 @@ use hyprland::data::{Client, Clients};
 use hyprland::data::{Workspace, Workspaces};
 //use hyprland::event_listener::{EventListenerMutable as EventListener, State, WindowOpenEvent};
 use hyprland::prelude::*;
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
+use std::collections::BTreeMap;
 
 pub mod prelude {}
 
-#[derive(Serialize)]
+#[derive(Serialize, Ord, Eq, PartialEq, PartialOrd, Clone)]
 struct SimpleWindow {
     id: i32,
     windows: u16,
 }
 
-fn list_workspaces() {
-    let mut jsonfy = vec![];
+
+fn list_workspaces(num: usize) {
+    let mut windows = BTreeMap::new();
+    let mut jsonfy = Vec::with_capacity(num);
     match Workspaces::get() {
         Ok(result) => {
             //println!("{:?}", result);
-            result.iter().for_each(|f| {
-                jsonfy.push(SimpleWindow {
-                    id: f.id,
-                    windows: f.windows,
-                })
+            result.iter()
+                .filter(|w| w.id > 0 && w.id <= num as i32)
+                .for_each(|f| {
+                    windows.insert(f.id, SimpleWindow {
+                        id: f.id,
+                        windows: f.windows,
+                    });
             });
+
+            (0..=num).for_each(|id|{
+                match windows.get(&(id as i32)) {
+                   Some(window) => jsonfy.push(window.to_owned()),
+                   None => jsonfy.push(SimpleWindow{id: id as i32, windows: 0})
+                }
+            });
+
             println!(
                 "{}",
                 serde_json::to_string(&jsonfy).unwrap_or("[]".to_string())
             );
-        }
+        },
         Err(_) => {
             println!("{}", serde_json::json!([{"id":"1", "windows":0}]));
         }
     }
+
 }
 
 pub fn listen() {
-    list_workspaces();
+    list_workspaces(9);
 }
